978f32d847071c05e052b3223a6b8e9a0a976f05
swapnilpatil205 <swapnilpatil205@gmail.com>
2020-06-16 11:44:34 +0530

    first commit

---
+++ OurWebApp.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace OurWebApp.Dates.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void VerifyDatesSince()
+        {
+            Assert.AreEqual(38795, 38795);
+            Assert.AreEqual(88387, 88387);
+            Assert.AreEqual(347608, 347608);
+        }
+    }
+}
